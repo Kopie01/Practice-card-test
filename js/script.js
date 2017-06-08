a793df8f9a5814ce2755e6b448166d809e6709d3
kirsten opie <845841129@ken20-202-15.local>
2017-06-09 09:38:39 +1200

    changes to images and borders

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// code for changing title of event
+// code for changing title of event and image
 
 var greeting = document.getElementById("greeting");
 var image = document.getElementById("picture");
@@ -7,17 +7,18 @@ $("#christmas").click(function(){
 	console.log("christmas");
 	console.log(greeting);
 	greeting.innerHTML = "Merry Christmas!";
+	$("#picture").attr("src", "images/christmas.jpg");
 	
 	})
 
 $("#birthday").click(function(){
 	greeting.innerHTML = "Happy Birthday!";
-	
+	$("#picture").attr("src", "images/birthday.jpg");
 	})
 
 $("#mother").click(function(){
 	greeting.innerHTML = "Happy Mother's Day!";
-	
+	$("#picture").attr("src", "images/flowers.jpg");
 	})
 
 // code for changing background colour
@@ -42,14 +43,35 @@ $("#green").click(function(){
 	
 	})
 
-$("#red").click(function(){
-	console.log("red");
-	colour.style.backgroundColor = "#af0314";
-	
-	})
 
 $("#yellow").click(function(){
 	console.log("yellow");
 	colour.style.backgroundColor = "#f2b141";
 	
 	})
+
+// code for changing border style
+
+var borderTop = document.getElementById("picture");
+
+$("#double").click(function(){
+	console.log("picture");
+	borderTop.style.borderTop = "double 7px white";
+	borderTop.style.borderBottom = "double 7px white";
+})
+
+$("#solid").click(function(){
+	console.log("picture");
+	borderTop.style.borderTop = "solid 6px white";
+	borderTop.style.borderBottom = "solid 6px white";
+})
+
+$("#dotted").click(function(){
+	console.log("picture");
+	borderTop.style.borderTop = "dotted 6px white";
+	borderTop.style.borderBottom = "dotted 6px white";
+})
+
+// code for choosing font
+
+
